6d25b3eb7d7bcf67a373f079f3f2e9d69a95042a
Nikita Gruzdev <46005116+D2ntes@users.noreply.github.com>
2020-04-19 14:22:05 +0300

    fixed

--- apps/forms.py
+++ apps/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Article, New
+from .models import Article, New, Company
 
 from ckeditor.widgets import CKEditorWidget
 
@@ -15,12 +15,21 @@ class ReviewArticle(Review):
 
     class Meta:
         model = Article
-        fields = ['title', 'published_at', 'file']
-        verbose_name = 'Текст'
+        fields = ['title', 'published_at', 'file', ]
+
+
 
 class ReviewNew(Review):
 
     class Meta:
         model = New
-        fields = ['title', 'published_at', 'file', 'image']
-        verbose_name = 'Текст'
+        fields = ['title', 'published_at', 'file', 'image', ]
+
+
+class ReviewCompany(forms.ModelForm):
+    about = forms.CharField(widget=CKEditorWidget())
+    important = forms.CharField(widget=CKEditorWidget())
+
+    class Meta:
+        model = Company
+        fields = ['name', 'address', 'tel', 'fax', 'email', 'about', 'important', ]
