25fa6f9507e2840d3d61fba2ca77817464eb8675
Nikita Gruzdev <46005116+D2ntes@users.noreply.github.com>
2020-05-04 13:27:39 +0300

    minor changes

--- nnte/urls.py
+++ nnte/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from apps.views import index, news, category, new, article, vacancy, vacancies, back_url
+from apps.views import index, news, category, new, article, vacancy, vacancies
 from django.conf.urls.static import static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf import settings
